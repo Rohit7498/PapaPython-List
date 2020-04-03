c9218cc031c2ab4000e0e75e93a7c55229f52e03
Rohit7498 <rohitKumar20163127@gmail.com>
2020-04-03 21:35:21 +0530

    Added frontend and requirements text file

--- CragApp/urls.py
+++ CragApp/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('new_search', views.new_search, name='new_search'),
+    
 
 ]
--- CragApp/views.py
+++ CragApp/views.py
@@ -1,7 +1,22 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'CragApp/base.html')
+    return render(request, 'base.html')
+
+
+def new_search(request):
+
+    search = request.POST.get('search')
+    #print(search)
+
+    stuff_for_frontend = {
+                            'search':search,
+                            }
+
+    return render(request, 'CragApp/new_search.html', stuff_for_frontend)
+
